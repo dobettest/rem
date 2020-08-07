1046847d6157ad0e9e678884b7fde19b3a0195af
dobettest <1364525216@qq.com>
2020-08-07 17:28:14 +0800

    20200807

---
+++ src/index.js
@@ -0,0 +1,9 @@
+require("../index.scss")
+//获取屏幕宽度(viewport)
+let htmlWidth=document.documentElement.clientWidth||document.body.clientWidth
+var htmlDom=document.getElementsByTagName("html")[0]
+htmlDom.style.fontSize=(htmlWidth / 10)+'px';
+window.addEventListener('resize',(e)=>{
+    let htmlWidth=document.documentElement.clientWidth||document.body.clientWidth
+    htmlDom.style.fontSize=(htmlWidth / 10)+'px';
+})
